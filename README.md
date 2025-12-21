a4db32ac0d1687d777480291531c49cdbecb839d
Brett Porter <49108+brettporter@users.noreply.github.com>
2025-12-21 18:19:49 +1100

    day 4, part 2

--- README.md
+++ README.md
@@ -13,8 +13,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | :---: | :---: | :---:  |
 | [Day 2](./src/bin/02.rs) | `5.3µs` | `27.3µs` |
 | [Day 3](./src/bin/03.rs) | `66.4µs` | `1.1ms` |
+| [Day 4](./src/bin/04.rs) | `450.6µs` | `5.4ms` |
 
-**Total: 1.20ms**
+**Total: 7.05ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/04.rs
+++ src/bin/04.rs
@@ -16,7 +16,12 @@ fn parse_input(input: &str) -> IResult<&str, Vec<Vec<char>>> {
 pub fn part_one(input: &str) -> Option<u64> {
     let (_, grid) = parse_input(input).unwrap();
 
-    let mut accessible = 0;
+    let accessible = find_accessible(&grid);
+    Some(accessible.len() as u64)
+}
+
+fn find_accessible(grid: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
+    let mut accessible = vec![];
     for row in 0..grid.len() {
         let row_length = grid[row].len();
         for col in 0..row_length {
@@ -51,17 +56,35 @@ pub fn part_one(input: &str) -> Option<u64> {
                     surrounding_count += 1;
                 }
                 if surrounding_count < 4 {
-                    accessible += 1;
+                    accessible.push((row, col));
                 }
             }
         }
     }
 
-    Some(accessible)
+    accessible
+}
+
+fn count_total_accessible(grid: &Vec<Vec<char>>) -> u64 {
+    let accessible = find_accessible(grid);
+    let accessible_count = accessible.len();
+    if accessible.is_empty() {
+        return 0;
+    }
+
+    let mut new_grid: Vec<Vec<char>> = grid.iter().map(|r| r.clone()).collect();
+
+    for (row, col) in accessible {
+        new_grid[row][col] = '.';
+    }
+
+    accessible_count as u64 + count_total_accessible(&new_grid)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (_, grid) = parse_input(input).unwrap();
+
+    Some(count_total_accessible(&grid))
 }
 
 #[cfg(test)]
@@ -77,6 +100,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(43));
     }
 }
