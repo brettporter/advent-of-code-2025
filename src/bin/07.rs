e3847cd7755c2c85038bde979239d5296ae99fa0
Brett Porter <49108+brettporter@users.noreply.github.com>
2025-12-22 01:22:55 +1100

    day 7, part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,4 +1,4 @@
-use fxhash::FxHashSet;
+use fxhash::{FxHashMap, FxHashSet};
 use nom::{
     IResult, Parser,
     character::complete::{newline, one_of},
@@ -29,7 +29,6 @@ pub fn part_one(input: &str) -> Option<u64> {
             if r[beam_col] == '^' {
                 let loc = (row, beam_col);
                 if !split_beams.contains(&loc) {
-                    // TODO: check edges if needed
                     beams_to_process.push((row, beam_col - 1));
                     beams_to_process.push((row, beam_col + 1));
                     split_beams.insert(loc);
@@ -43,7 +42,36 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let (_, rows) = parse_input(input).unwrap();
+
+    // assume start is in first row
+    let start = (0usize, rows[0].iter().position(|&x| x == 'S').unwrap());
+    let mut memo = FxHashMap::default();
+
+    Some(count_timelines(start, &rows, &mut memo))
+}
+
+fn count_timelines(
+    start: (usize, usize),
+    rows: &Vec<Vec<char>>,
+    memo: &mut FxHashMap<(usize, usize), u64>,
+) -> u64 {
+    if let Some(&count) = memo.get(&start) {
+        return count;
+    }
+
+    let (beam_row, beam_col) = start;
+
+    for (idx, r) in rows[beam_row + 1..].iter().enumerate() {
+        let row = beam_row + idx + 1;
+        if r[beam_col] == '^' {
+            let timelines = count_timelines((row, beam_col - 1), rows, memo)
+                + count_timelines((row, beam_col + 1), rows, memo);
+            memo.insert(start, timelines);
+            return timelines;
+        }
+    }
+    return 1;
 }
 
 #[cfg(test)]
@@ -59,6 +87,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(40));
     }
 }
