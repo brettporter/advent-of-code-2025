7dcc97cb0cfe1b7c648e94fb0a1d1d89d2086319
Brett Porter <49108+brettporter@users.noreply.github.com>
2025-12-28 00:50:34 +1100

    attempt using combinations, too slow

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -80,54 +80,100 @@ pub fn part_two(input: &str) -> Option<u64> {
 
     let mut total = 0;
     for (_, wires, joltage_requirements) in machines {
-        total += calculate_min_joltage_presses(&joltage_requirements, &wires);
+        total += calculate_min_joltage_presses(&joltage_requirements, &wires)?;
     }
 
     Some(total)
 }
 
-fn calculate_min_joltage_presses(joltage_requirements: &Vec<u64>, buttons: &[Vec<u64>]) -> u64 {
-    let mut sequences = FxHashMap::from_iter(buttons.iter().map(|v| {
-        let mut result = vec![0; joltage_requirements.len()];
-        for &idx in v {
-            result[idx as usize] = 1;
-        }
+fn calculate_min_joltage_presses(
+    joltage_requirements: &Vec<u64>,
+    buttons: &[Vec<u64>],
+) -> Option<u64> {
+    let button_max_count = buttons
+        .iter()
+        .map(|b| {
+            let count = b
+                .iter()
+                .map(|&i| joltage_requirements[i as usize])
+                .min()
+                .unwrap();
+            (b, count)
+        })
+        .collect::<Vec<_>>();
 
-        (result, 1)
-    }));
+    let projected = button_max_count
+        .iter()
+        .fold(1, |acc, (_, count)| acc * count);
 
-    loop {
-        let mut new_sequences = sequences.clone();
-        for (result, num_presses) in &sequences {
-            for (s_result, s_num_presses) in &sequences {
-                let new_result: Vec<u64> = result
-                    .iter()
-                    .enumerate()
-                    .map(|(i, r)| s_result[i] + r)
-                    .collect();
-                let new_num_presses = num_presses + s_num_presses;
+    println!(
+        "Projected min presses: {} for {:?}",
+        projected, joltage_requirements
+    );
 
-                if new_result
-                    .iter()
-                    .enumerate()
-                    .all(|(i, v)| *v <= joltage_requirements[i])
-                {
-                    if let Some(v) = new_sequences.get(&new_result) {
-                        if *v > new_num_presses {
-                            new_sequences.insert(new_result, new_num_presses);
-                        }
-                    } else {
-                        new_sequences.insert(new_result, new_num_presses);
-                    }
-                }
+    let result = vec![0; joltage_requirements.len()];
+    let mut count = 0;
+    let r = try_button_ranges(
+        &button_max_count,
+        &joltage_requirements,
+        0,
+        &result,
+        &mut count,
+    );
+    println!("counted {} calls", count);
+    r
+}
+
+fn try_button_ranges(
+    button_ranges: &[(&Vec<u64>, u64)],
+    joltage_requirements: &Vec<u64>,
+    presses: u64,
+    result: &[u64],
+    c: &mut u64,
+) -> Option<u64> {
+    let mut best = None;
+
+    let &(b, count) = button_ranges.first()?;
+    for i in 0..=count {
+        *c += 1;
+        // println!(
+        //     "Trying button range {:?} with count {} on result {:?} ({i})",
+        //     b, count, result
+        // );
+        let mut new_result = Vec::from(result);
+        for idx in b {
+            let idx = *idx as usize;
+            new_result[idx] += i;
+            if new_result[idx] > joltage_requirements[idx] {
+                // println!("Too high at index {}: {}", idx, new_result[idx]);
+                return best;
             }
         }
-        if let Some(r) = new_sequences.get(joltage_requirements) {
-            return *r;
+        let new_presses = presses + i;
+
+        if new_result == *joltage_requirements {
+            // println!(
+            // "Matched with presses: {} on {:?}",
+            // new_presses, joltage_requirements
+            // );
+            if new_presses < best.unwrap_or(u64::MAX) {
+                best = Some(new_presses);
+            }
         }
 
-        sequences = new_sequences;
+        if let Some(r) = try_button_ranges(
+            &button_ranges[1..],
+            joltage_requirements,
+            new_presses,
+            &new_result,
+            c,
+        ) {
+            if r < best.unwrap_or(u64::MAX) {
+                best = Some(r);
+            }
+        }
     }
+    best
 }
 
 #[cfg(test)]
@@ -144,5 +190,22 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(33));
+    }
+
+    #[test]
+    fn test_calculate_min_presses() {
+        assert_eq!(
+            calculate_min_joltage_presses(
+                &vec![7, 5, 12, 7, 2],
+                &[
+                    vec![0, 2, 3, 4],
+                    vec![2, 3],
+                    vec![0, 4],
+                    vec![0, 1, 2],
+                    vec![1, 2, 3, 4]
+                ]
+            ),
+            Some(12)
+        );
     }
 }
