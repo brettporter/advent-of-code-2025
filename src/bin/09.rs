961788d52384b960b8a9df767e96d72af4d12858
Brett Porter <49108+brettporter@users.noreply.github.com>
2025-12-27 13:41:33 +1100

    another attempt

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -32,99 +32,81 @@ pub fn part_one(input: &str) -> Option<i64> {
         .max()
 }
 
-fn intersects_line(tiles: &[(i64, i64)], p1: (i64, i64), p2: (i64, i64)) -> bool {
-    let horiz = p1.1 == p2.1;
+fn is_inside_polygon(tiles: &[(i64, i64)], (p_x, p_y): (i64, i64)) -> bool {
+    // simplified version of point-in-polygon algorithm based on assumption of horizontal and vertical lines
+    // and no internal gaps. Usually ray casting would be an odd number of intersections from a point outside
+    // the polygon. However, we can just test that if there are lines in every direction from the point, it
+    // is inside
 
-    let mut adjust_p1 = p1;
-    let mut adjust_p2 = p2;
-    for (idx, next) in tiles.iter().enumerate() {
-        // TODO: change p1 to be the segment not on the line if any end matches
-    }
-
-    let p1 = adjust_p1;
-    let p2 = adjust_p2;
-
-    for (idx, next) in tiles.iter().enumerate() {
-        let prev = if idx > 0 {
+    // Iterate all lines on the polygon
+    let (mut left, mut right, mut up, mut down) = (false, false, false, false);
+    for (idx, &(next_x, next_y)) in tiles.iter().enumerate() {
+        let &(prev_x, prev_y) = if idx > 0 {
             &tiles[idx - 1]
         } else {
             tiles.last().unwrap()
         };
 
-        // getting caught here where lines intersect only at the corners of the polygon
-        // I think we need to remove full line segments first
+        if prev_y == next_y {
+            // horizontal - test up / down
 
-        if prev.1 == next.1 && !horiz {
-            // horizontal
-            let y = prev.1;
-            let (x1, x2) = (min(prev.0, next.0), max(prev.0, next.0));
-            let (line_y1, line_y2) = (min(p1.1, p2.1), max(p2.1, p1.1));
-            let line_x = p1.0;
-
-            if x1 <= line_x && line_x <= x2 && line_y1 <= y && y <= line_y2 {
-                // ignore if it's an origin point
-                if line_x != p1.0 || y != p1.1 {
-                    println!(
-                        "intersect horiz {:?} {:?} :: {:?} {:?} @ {line_x} {y}",
-                        prev, next, p1, p2
-                    );
-                    return true;
+            let (x1, x2) = (min(prev_x, next_x), max(prev_x, next_x));
+            if x1 <= p_x && p_x <= x2 {
+                if p_y >= prev_y {
+                    println!("U -> {p_x}, {p_y} below ({x1}-{x2}, {prev_y})");
+                    up = true;
+                }
+                if p_y <= prev_y {
+                    println!("D -> {p_x}, {p_y} above ({x1}-{x2}, {prev_y})");
+                    down = true;
                 }
             }
         }
 
-        if prev.0 == next.0 && horiz {
-            // vertical
-            let x = prev.0;
-            let (y1, y2) = (min(prev.1, next.1), max(prev.1, next.1));
-            let (line_x1, line_x2) = (min(p1.0, p2.0), max(p2.0, p1.0));
-            let line_y = p1.1;
+        if prev_x == next_x {
+            // vertical - test left/right
 
-            if y1 <= line_y && line_y <= y2 && line_x1 <= x && x <= line_x2 {
-                // ignore if it's an origin point
-                if x != p1.0 || line_y != p1.1 {
-                    println!(
-                        "intersect vert {:?} {:?} :: {:?} {:?} @ {x} {line_y}",
-                        prev, next, p1, p2
-                    );
-                    return true;
+            let (y1, y2) = (min(prev_y, next_y), max(prev_y, next_y));
+            if y1 <= p_y && p_y <= y2 {
+                if p_x >= prev_x {
+                    println!("L -> {p_x}, {p_y} right of ({prev_x}, {y1}-{y2})");
+                    left = true;
+                }
+                if p_x <= prev_x {
+                    println!("R -> {p_x}, {p_y} left of ({prev_x}, {y1}-{y2})");
+                    right = true;
                 }
             }
+        }
+        if left && right && up && down {
+            return true;
         }
     }
+    println!("Failed on point ({p_x}, {p_y}) - L {left} R {right} U {up} D {down}");
     false
 }
 
 pub fn part_two(input: &str) -> Option<i64> {
     let (_, tiles) = parse_input(input).unwrap();
 
-    let sorted: Vec<(Vec<&(i64, i64)>, i64)> = tiles
+    let sorted: Vec<((i64, i64), (i64, i64), i64)> = tiles
         .iter()
         .combinations(2)
         .map(|v| {
-            let (x1, y1) = v[0];
-            let (x2, y2) = v[1];
+            let &(x1, y1) = v[0];
+            let &(x2, y2) = v[1];
             let h = (y2 - y1).abs() + 1;
             let w = (x2 - x1).abs() + 1;
-            (v, w * h)
+            ((x1, y2), (x2, y1), w * h)
         })
-        .sorted_by(|a, b| b.1.cmp(&a.1))
+        .sorted_by(|(_, _, a), (_, _, b)| b.cmp(&a))
         .collect();
 
-    for (v, a) in sorted {
-        let (x1, y1) = (v[0].0, v[0].1);
-        let (x2, y2) = (v[1].0, v[1].1);
-
-        println!("Rect {:?} {:?}", (x1, y1), (x2, y2));
-
-        if intersects_line(&tiles, (x1, y1), (x2, y1))
-            || intersects_line(&tiles, (x2, y1), (x2, y2))
-            || intersects_line(&tiles, (x2, y2), (x1, y2))
-            || intersects_line(&tiles, (x1, y2), (x1, y1))
-        {
-            continue;
+    for ((x1, y1), (x2, y2), a) in sorted {
+        println!("Testing rectangle ({x1}, {y1}), ({x2}, {y2}) size {a}");
+        if is_inside_polygon(&tiles, (x1, y1)) && is_inside_polygon(&tiles, (x2, y2)) {
+            return Some(a);
         }
-        return Some(a);
     }
     None
 }
